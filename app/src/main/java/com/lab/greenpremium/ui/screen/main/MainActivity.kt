909c248071ad6f3a134993269db652c27be4b54c
Epifanov Konstantin <epifanov.ka@spb.rusoft-company.ru>
2018-07-17 18:32:39 +0300

    main screen st.1

--- app/src/main/java/com/lab/greenpremium/ui/screen/main/MainActivity.kt
+++ app/src/main/java/com/lab/greenpremium/ui/screen/main/MainActivity.kt
@@ -1,23 +1,38 @@
 package com.lab.greenpremium.ui.screen.main
 
+import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
+import com.lab.greenpremium.R
 import com.lab.greenpremium.ui.base.BaseActivity
 
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
 
-/*    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
-            R.id.navigation_home -> {
-                message.setText(R.string.title_home)
+            R.id.nav_plants -> {
+                message.setText(R.string.title_plants)
                 return@OnNavigationItemSelectedListener true
             }
-            R.id.navigation_dashboard -> {
-                message.setText(R.string.title_dashboard)
+
+            R.id.nav_portfolio -> {
+                message.setText(R.string.title_portfolio)
                 return@OnNavigationItemSelectedListener true
             }
-            R.id.navigation_notifications -> {
-                message.setText(R.string.title_notifications)
+
+            R.id.nav_profile -> {
+                message.setText(R.string.title_profile)
+                return@OnNavigationItemSelectedListener true
+            }
+
+            R.id.nav_contacts -> {
+                message.setText(R.string.title_contacts)
+                return@OnNavigationItemSelectedListener true
+            }
+
+            R.id.nav_map -> {
+                message.setText(R.string.title_map)
                 return@OnNavigationItemSelectedListener true
             }
         }
@@ -28,6 +43,7 @@ class MainActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        navigation.itemIconTintList = null
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
-    }*/
+    }
 }
