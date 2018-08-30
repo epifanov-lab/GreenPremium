0d5ccced6ab4958dc3002a47a52b2b3cc32bf58d
Epifanov Konstantin <epifanov.konstantin.lab@gmail.com>
2018-08-30 22:10:24 +0300

    plant detail screen st.1

---
+++ app/src/main/java/com/lab/greenpremium/ui/screen/plant_detail/PlantDetailActivity.kt
@@ -0,0 +1,13 @@
+package com.lab.greenpremium.ui.screen.plant_detail
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.lab.greenpremium.R
+
+class PlantDetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_plant_detail)
+    }
+}
